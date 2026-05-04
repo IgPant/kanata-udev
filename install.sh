86d03f0ff0be070742b63b3e76b45fd8a3644a97
Igor Antunes <igorpiresantunes@gmail.com>
2026-05-03 23:13:54 -0300

    Support multiple keyboards: add ID75 (6964:0075), update install script for multi-keyboard setup

--- install.sh
+++ install.sh
@@ -3,16 +3,28 @@ set -e
 
 KANATA_BIN="$(pwd)/kanata"
 KANATA_CFG="$(pwd)/kanata.kbd"
-SCRIPTS_DIR="$(dirname "$(readlink -f "$0")")"
 
 echo "=== Kanata USB Keyboard Automator Installer ==="
 echo ""
-echo "Current keyboards connected:"
-lsusb | grep -i keyboard || lsusb
+echo "Current USB devices:"
+lsusb
 echo ""
 
-read -p "Enter keyboard USB vendor ID (e.g. 5262): " VENDOR_ID
-read -p "Enter keyboard USB product ID (e.g. 4e4b): " PRODUCT_ID
+KEYBOARDS=()
+echo "Add keyboards one by one. Leave vendor ID empty to finish."
+while true; do
+    read -p "Enter keyboard USB vendor ID (e.g. 5262) [enter to stop]: " VENDOR_ID
+    [ -z "$VENDOR_ID" ] && break
+    read -p "Enter keyboard USB product ID (e.g. 4e4b): " PRODUCT_ID
+    KEYBOARDS+=("$VENDOR_ID:$PRODUCT_ID")
+    echo "Added $VENDOR_ID:$PRODUCT_ID"
+done
+
+if [ ${#KEYBOARDS[@]} -eq 0 ]; then
+    echo "No keyboards added. Exiting."
+    exit 1
+fi
+
 read -p "Enter kanata binary path [$KANATA_BIN]: " KANATA_BIN_INPUT
 read -p "Enter kanata config path [$KANATA_CFG]: " KANATA_CFG_INPUT
 
@@ -59,20 +71,30 @@ EOF
 
 chmod +x "$BIN_DIR/udev-kanata-start.sh" "$BIN_DIR/udev-kanata-stop.sh"
 
+UDEV_RULE=""
+for KB in "${KEYBOARDS[@]}"; do
+    VID="${KB%%:*}"
+    PID="${KB##*:}"
+    HID_VID=$(printf "%04x" "0x$VID")
+    HID_PID=$(printf "%04x" "0x$PID")
+    UDEV_RULE+="
+ACTION==\"remove\", SUBSYSTEM==\"input\", ENV{ID_VENDOR_ID}==\"$VID\", ENV{ID_MODEL_ID}==\"$PID\", RUN+=\"$BIN_DIR/udev-kanata-start.sh\"
+ACTION==\"remove\", SUBSYSTEM==\"hid\", ENV{HID_ID}==\"0003:0000${HID_VID^^}:0000${HID_PID^^}\", RUN+=\"$BIN_DIR/udev-kanata-start.sh\"
+ACTION==\"remove\", SUBSYSTEM==\"usb\", DEVTYPE==\"usb_device\", ATTR{idVendor}==\"$VID\", ATTR{idProduct}==\"$PID\", RUN+=\"$BIN_DIR/udev-kanata-start.sh\"
+
+ACTION==\"add\", SUBSYSTEM==\"input\", ENV{ID_VENDOR_ID}==\"$VID\", ENV{ID_MODEL_ID}==\"$PID\", RUN+=\"$BIN_DIR/udev-kanata-stop.sh\"
+ACTION==\"add\", SUBSYSTEM==\"hid\", ENV{HID_ID}==\"0003:0000${HID_VID^^}:0000${HID_PID^^}\", RUN+=\"$BIN_DIR/udev-kanata-stop.sh\"
+ACTION==\"add\", SUBSYSTEM==\"usb\", DEVTYPE==\"usb_device\", ATTR{idVendor}==\"$VID\", ATTR{idProduct}==\"$PID\", RUN+=\"$BIN_DIR/udev-kanata-stop.sh\"
+"
+done
+
 echo ""
-echo "Generated udev rule (needs sudo to install):"
-echo ""
-cat << RULE
-ACTION=="remove", SUBSYSTEM=="usb", DEVTYPE=="usb_device", ATTR{idVendor}=="$VENDOR_ID", ATTR{idProduct}=="$PRODUCT_ID", RUN+="$BIN_DIR/udev-kanata-start.sh"
-ACTION=="add", SUBSYSTEM=="usb", DEVTYPE=="usb_device", ATTR{idVendor}=="$VENDOR_ID", ATTR{idProduct}=="$PRODUCT_ID", RUN+="$BIN_DIR/udev-kanata-stop.sh"
-RULE
+echo "Generated udev rule:"
+echo "$UDEV_RULE"
 
 read -p "Install udev rule now? (requires sudo) [Y/n]: " INSTALL
 if [ "${INSTALL:-Y}" = "Y" ] || [ "$INSTALL" = "y" ]; then
-    pkexec tee /etc/udev/rules.d/90-kanata-keyboard.rules << RULE
-ACTION=="remove", SUBSYSTEM=="usb", DEVTYPE=="usb_device", ATTR{idVendor}=="$VENDOR_ID", ATTR{idProduct}=="$PRODUCT_ID", RUN+="$BIN_DIR/udev-kanata-start.sh"
-ACTION=="add", SUBSYSTEM=="usb", DEVTYPE=="usb_device", ATTR{idVendor}=="$VENDOR_ID", ATTR{idProduct}=="$PRODUCT_ID", RUN+="$BIN_DIR/udev-kanata-stop.sh"
-RULE
+    echo "$UDEV_RULE" | pkexec tee /etc/udev/rules.d/90-kanata-keyboard.rules > /dev/null
     pkexec udevadm control --reload-rules
     echo "Done! udev rules installed and reloaded."
 else
